6f8bbfd39a1f6a4a0014dd0da0d9147e577d836b
taauntik <devauntik@gmail.com>
2022-01-23 13:03:23 +0600

    done Navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { render } from "react-dom";
 
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.scss";
 
 render(<App />, document.getElementById("root"));
